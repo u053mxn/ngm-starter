8cb2bb2756bcd6084fa7b341940b6f51ef096433
u027mxn <michael.nelms2@carlsonwagonlit.com>
2019-04-19 14:21:36 -0500

    Change name refs

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage/e2-mod-dash'),
+      dir: require('path').join(__dirname, '../coverage/ngm-starter'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
